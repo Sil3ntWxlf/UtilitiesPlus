7358bcbd0a3cf28b4b763b55609ebf09485f4eb6
Sil3ntWxlf <sil3ntwxlf@gmail.com>
2020-08-04 15:46:18 -0700

    init files

---
+++ src/main/java/io/github/sil3ntwxlf/UtilitiesPlus.java
@@ -0,0 +1,16 @@
+package io.github.sil3ntwxlf;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class UtilitiesPlus extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
