0eccedce2e7a0140670f4cd67a8c72cdbfbf44f1
Sil3ntWxlf <sil3ntwxlf@gmail.com>
2020-08-04 17:13:04 -0700

    Added LuckPerms & bStats api to dependencies + servers pie chart.

--- src/main/java/io/github/sil3ntwxlf/UtilitiesPlus.java
+++ src/main/java/io/github/sil3ntwxlf/UtilitiesPlus.java
@@ -1,5 +1,6 @@
 package io.github.sil3ntwxlf;
 
+import org.bstats.bukkit.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class UtilitiesPlus extends JavaPlugin {
@@ -7,6 +8,11 @@ public final class UtilitiesPlus extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        int pluginId = 8424; // <-- Replace with the id of your plugin!
+        Metrics metrics = new Metrics(this, pluginId);
+
+        // Optional: Add custom charts
+        metrics.addCustomChart(new Metrics.SimplePie("chart_id", () -> "servers"));
 
     }
     @Override
