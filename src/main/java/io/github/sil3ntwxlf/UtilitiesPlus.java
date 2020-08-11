4fa59a7d0befcd689c95ce4465f6a3cd4e167dc7
Sil3ntWxlf <sil3ntwxlf@gmail.com>
2020-08-11 04:55:18 -0700

    Added base foundation for config.json & created initial CommandExecutor for GamemodeAliasesCommand.java

--- src/main/java/io/github/sil3ntwxlf/UtilitiesPlus.java
+++ src/main/java/io/github/sil3ntwxlf/UtilitiesPlus.java
@@ -1,10 +1,24 @@
 package io.github.sil3ntwxlf;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import io.github.sil3ntwxlf.commands.GamemodeAliasesCommand;
 import org.bstats.bukkit.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.HashMap;
+import java.util.Map;
+
+
 public final class UtilitiesPlus extends JavaPlugin {
+    public /*static*/ final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    public static HashMap<String, Object> map = new HashMap<>();
 
     @Override
     public void onEnable() {
@@ -12,14 +26,39 @@ public final class UtilitiesPlus extends JavaPlugin {
         int pluginId = 8424; // <-- Replace with the id of your plugin!
         Metrics metrics = new Metrics(this, pluginId);
 
+        //HashMap onLoad
+        map.put("onLoad", "Load");
+        System.out.println("UtilitiesPlus has been loaded!"+ map.get("onLoad"));
+
         // Optional: Add custom charts
         metrics.addCustomChart(new Metrics.SimplePie("chart_id", () -> "servers"));
 
         //Command Aliases Ahoy
         getCommand("gamemode").setExecutor(new GamemodeAliasesCommand());
+
+        //Configuration Generation
+        try {
+            File defaultConfigFile = new File(getClass().getClassLoader().getResource("config.json").getFile());
+            File configFile = new File(getDataFolder(), "config.json");
+            if ((configFile.getParentFile() != null && configFile.getParentFile().mkdirs()) || configFile.createNewFile()) {
+                configFile.createNewFile();
+                BufferedWriter bw = new BufferedWriter(new FileWriter(configFile));
+                bw.write(new String(Files.readAllBytes(Paths.get(defaultConfigFile.toURI()))));
+                bw.close();
+            }
+            String rawContent = new String(Files.readAllBytes(Paths.get(configFile.toURI())));
+            JsonObject json = new JsonParser().parse(rawContent).getAsJsonObject();
+            System.out.println(rawContent);
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
     }
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+
+        //HashMap onUnload
+        map.put("onUnload", "Unload");
+        System.out.println("UtilitiesPlus has been disabled!"+ map.get("onUnload"));
     }
 }
